e69c6ec5b3af275ecd3e2917c37c95d01b8ff207
Nishan Chathuranga Wickramarathna <inbox.nishan@gmail.com>
2017-11-04 13:02:46 +0530

    Add project files.

---
+++ ConstructionLK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConstructionLK.Startup))]
+namespace ConstructionLK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
